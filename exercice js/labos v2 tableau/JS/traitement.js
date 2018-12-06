a64ed297d0e38a5931472c4fcae661741cbc8aec
cotni1832641 <irmortal123@gmail.com>
2018-12-06 00:57:52 -0500

    labos v2 fini

--- exercice js/labos v2 tableau/JS/traitement.js
+++ exercice js/labos v2 tableau/JS/traitement.js
@@ -1,3 +1,7 @@
+var TabNom = new Array(4);
+var TabPrenom = new Array(4);
+var TabAge = new Array(4);
+var cptJoueur=0;
 function frmMembre_onSubmit()
 {
     var valid=true;
@@ -6,7 +10,7 @@ function frmMembre_onSubmit()
 
         if(format()===true)
         {
-
+            btnAjouter_onclick();
             if (confirm("voulez-vous valider votre inscription, le cout seras de :"+traiterInfo()+" $") == false)
             {
                 valid=false;
@@ -37,6 +41,68 @@ function traiterInfo()
         Prix=80;
     }
 return prix;
+}
+function btnAjouter_onclick()
+{
+    var Age=parseFloat(document.getElementById("txtAge").value);
+    var Nom=document.getElementById("txtNom").value;
+    var Prenom=document.getElementById("txtPrenom").value;
+    TabPrenom[cptJoueur]=Prenom;
+    TabNom[cptJoueur]=Nom;
+    TabAge[cptJoueur]=Age;
+    cptJoueur++;
+    ActiverOption(cptJoueur);
+}
+function ActiverOption(cpt)
+{
+    if(cpt>4)
+    {
+        document.getElementById("btnTrouverPlusJeune").disabled=false;
+        document.getElementById("btnTrouverNomComplet").disabled=false;
+    }
+
+}
+function trouverPlusJeune()
+{
+    var AgePlusJeune=200,Indicateur;
+    for(var i=0;i<TabNom.length;i++)
+    {
+        if(TabAge[i]<AgePlusJeune)
+        {
+            AgePlusJeune=TabAge[i];
+            Indicateur=i;
+        }
+    }
+    return Indicateur;
+}
+function btnTrouverPlusJeune_onclick()
+{
+    document.getElementById("lblReponse").innerHTML="le nom du plus jeune est: "+TabPrenom[trouverPlusJeune()]+" "+TabNom[trouverPlusJeune()]+". Son Age est de: "+TabAge[trouverPlusJeune()];
+}
+function validNomCompletExist()
+{
+    var validNomExist=false;
+    var cpt=0;
+    while(cpt<TabAge.length && validNomExist===false)
+    {
+        if(TabNom[cpt]===document.getElementById("txtNom").value && TabPrenom[cpt]===document.getElementById("txtPrenom").value)
+        {
+            validNomExist=true
+        }
+        else
+            cpt++;
+    }
+    return validNomExist;
+}
+function btnTrouverNomComplet_onclick()
+{
+    if(validNomCompletExist()===true)
+    {
+        document.getElementById("lblReponse").innerHTML="le prénom existe bien";
+    }
+    else
+        document.getElementById("lblReponse").innerHTML="le prénom existe Pas";
+
 }
 function validChampsObli()
 {
